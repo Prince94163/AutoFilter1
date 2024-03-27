964fdbe2cba111b6c0816a1d909c9f736a593021
Prince94163 <128693609+Prince94163@users.noreply.github.com>
2024-03-27 21:51:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,16 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/erotixe/AutoFilter.git /AutoFilter
+  git clone https://github.com/Prince94163/AutoFilter1 /AutoFilter1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilter
+  git clone $UPSTREAM_REPO /AutoFilter1
 fi
-cd /AutoFilter
+cd /AutoFilter1
 pip3 install -U -r requirements.txt
+pip3 install spotdl
+pip3 install yt-dlp
+pip3 install speedtest
+
 echo "Starting Bot...."
 python3 bot.py
